2c83f29354060b0eb2d9af63f1e513ee3f4d5a34
B. Petersen <r10s@b44t.com>
2014-12-16 13:05:53 +0100

    textarea heights can be configured in the settings

--- admin/lib/control/textarea.inc.php
+++ admin/lib/control/textarea.inc.php
@@ -4,11 +4,19 @@ class CONTROL_TEXTAREA_CLASS extends CONTROL_TEXT_CLASS // not: CONTROL_BASE_CLA
 {
 	public function render_html($addparam)
 	{
+		// get the number of rows to display initially
+		// (the intial number can be modified in the settings and the the global config, for temporary sizing of the textarea, we rely on the browser)
 		$rows = 5;
 		if( $this->row_def->prop['ctrl.rows'] ) {
 			$rows = intval($this->row_def->prop['ctrl.rows']);
 		}
-	
+		
+		$temp = str_replace(' ', '', regGet("edit.field.{$this->table_def->name}.{$this->row_def->name}.size", "40 x $rows"));
+		list($dummy, $rows) = explode('x', $temp);
+		$rows = intval($rows);
+		if( $rows < 2 ) $rows = 2;
+		if( $rows > 32 ) $rows = 32;
+								
 		return	"<textarea name=\"{$this->name}\" style=\"width: 90%; max-width:900px;\" rows=\"$rows\"".$this->tooltip_attr().$this->readonly_attr().">"
 			.		isohtmlspecialchars($this->dbval)
 			.	"</textarea>";
