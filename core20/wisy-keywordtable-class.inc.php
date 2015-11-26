d825f320c05d73b2af8da88031741637ea2982fc
B. Petersen <r10s@b44t.com>
2015-11-26 22:17:35 +0100

    Increase keyword-ratgeber version to invalidate caches

--- core20/wisy-keywordtable-class.inc.php
+++ core20/wisy-keywordtable-class.inc.php
@@ -197,7 +197,7 @@ class WISY_KEYWORDTABLE_CLASS
 	public function getHtml()
 	{
 		// is the result in the cache?
-		$cacheVersion = 'v5';
+		$cacheVersion = 'v6';
 		$cacheKey = "wisykwt.$cacheVersion.".$GLOBALS['wisyPortalId'].".$this->args".".".WISY_KEYWORDTABLE_CLASS::$sw_modified;
 		if( ($ret=$this->dbCache->lookup($cacheKey))!='' && substr($_SERVER['HTTP_HOST'], -6)!='.local' ) {
 			$ret = str_replace('<div class="wisy_glskeytime">', '<div class="wisy_glskeytime">Cached, ', $ret);
