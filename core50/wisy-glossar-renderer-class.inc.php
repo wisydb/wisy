b7159a04f5537e8b89582551d1e743216c159de8
Daniel Bagel <db@dbagel.de>
2016-06-07 00:07:11 +0200

    some minor markup changes to glossar. Markup changes in search renderer for more semantic contact data (vcard), better pagination

--- core50/wisy-glossar-renderer-class.inc.php
+++ core50/wisy-glossar-renderer-class.inc.php
@@ -32,13 +32,15 @@ class WISY_GLOSSAR_RENDERER_CLASS
 	
 		$db = new DB_Admin;
 	
-		$db->query("SELECT begriff, erklaerung, wikipedia FROM glossar WHERE id=$glossar_id");
+		$db->query("SELECT begriff, erklaerung, wikipedia, date_created, date_modified FROM glossar WHERE id=$glossar_id");
 		if( !$db->next_record() )
 			$this->framework->error404();
 
-		$begriff = $db->f8('begriff');
-		$erklaerung = $db->f8('erklaerung');
-		$wikipedia = $db->f8('wikipedia');
+		$begriff 		= $db->f8('begriff');
+		$erklaerung 	= $db->f8('erklaerung');
+		$wikipedia 		= $db->f8('wikipedia');
+		$date_created	= $db->f8('date_created');
+		$date_modified	= $db->f8('date_modified');
 
 		// Wenn es keine Erklärung, aber eine Wikipedia-Seite gibt -> Weiterleitung auf die entspr. Wikipedia-Seite
 		if( $erklaerung == '' && $wikipedia != '' )
@@ -67,40 +69,45 @@ class WISY_GLOSSAR_RENDERER_CLASS
 				echo '<a class="wisyr_zurueck" href="javascript:history.back();">&laquo; Zur&uuml;ck</a>';
 				echo $this->framework->getLinkList('help.link', ' &middot; ');
 			echo '</p>';
-			echo '<h1>' . htmlspecialchars($begriff) . '</h1>';
+			echo '<h1 class="wisyr_glossartitel">' . htmlspecialchars($begriff) . '</h1>';
 			flush();
 	
 			// render entry
+			echo '<section class="wisyr_glossar_infos clearfix">';
 			
-			if( $erklaerung != '' )
-			{
-				$wiki2html =& createWisyObject('WISY_WIKI2HTML_CLASS', $this->framework, array('selfGlossarId'=>$glossar_id));
-				echo $wiki2html->run($erklaerung);
-			}
+				if( $erklaerung != '' )
+				{
+					$wiki2html =& createWisyObject('WISY_WIKI2HTML_CLASS', $this->framework, array('selfGlossarId'=>$glossar_id));
+					echo $wiki2html->run($erklaerung);
+				}
 			
-			// render wikipedia link
+				// render wikipedia link
 	
-			if( $wikipedia != '' )
-			{
-				$isB2b = (substr($wikipedia, 0, 4) == 'b2b:')? true : false;
+				if( $wikipedia != '' )
+				{
+					$isB2b = (substr($wikipedia, 0, 4) == 'b2b:')? true : false;
 				
-				echo '<p>';
-					echo 'Weitere Informationen zu diesem Thema finden Sie <a href="'.htmlspecialchars($this->getWikipediaUrl($wikipedia)).'" target="_blank">';
-						echo ' ' . ($isB2b? 'im Weiterbildungs-WIKI' : 'in der Wikipedia');
-					echo '</a>';
-				echo '</p>';
-			}
-	
+					echo '<p>';
+						echo 'Weitere Informationen zu diesem Thema finden Sie <a href="'.htmlspecialchars($this->getWikipediaUrl($wikipedia)).'" target="_blank">';
+							echo ' ' . ($isB2b? 'im Weiterbildungs-WIKI' : 'in der Wikipedia');
+						echo '</a>';
+					echo '</p>';
+				}
+				
+			echo '</section><!-- /.wisyr_glossar_infos -->';
 
-		echo '</div>';
+			echo '<footer class="wisy_glossar_footer">';
+				echo '<div class="wisyr_glossar_meta">';
+					echo 'Information erstellt am ' . $this->framework->formatDatum($date_created);
+					echo ', zuletzt ge&auml;ndert am ' . $this->framework->formatDatum($date_modified);
+					$copyrightClass =& createWisyObject('WISY_COPYRIGHT_CLASS', $this->framework);
+					$copyrightClass->renderCopyright($db, 'glossar', $glossar_id);
+				echo '</div><!-- /.wisyr_glossar_meta -->';
+			echo '</footer><!-- /.wisy_glossar_footer -->';
 
-		// copyright
+
+		echo "\n</div><!-- /#wisy_resultarea -->";
 		
-		$copyrightClass =& createWisyObject('WISY_COPYRIGHT_CLASS', $this->framework);
-		$copyrightClass->renderCopyright($db, 'glossar', $glossar_id);
-
-
-		// done		
 		echo $this->framework->getEpilogue();
 	}
 };
