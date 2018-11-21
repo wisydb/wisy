ffb9fc9bdd9afb3fbe9285d07654e444fe1b2d6e
Daniel Bagel <db@dbagel.de>
2018-11-21 13:58:41 +0100

    Add landingpage functions for "thema und ort". Refactor existing functions slightly. Add some more documentation.

--- core51/wisy-landingpage-renderer-class.inc.php
+++ core51/wisy-landingpage-renderer-class.inc.php
@@ -106,16 +106,28 @@ class WISY_LANDINGPAGE_RENDERER_CLASS
 		$this->db->free();
 		echo '</ul></section>';	
 	}
-		
+	
 	/*
-	 * Kursliste für einzelnen Ort ausgeben
+	 * Einzelnen Ort ausgeben
+	 */
+	function renderOrt($ort, $stadtteil='') {
+		if(trim($stadtteil) != '') {
+			echo '<li class="wisyr_landingpage_list_ort_stadtteil"><a href="/orte/' . urlencode($ort) . '/' . urlencode($stadtteil) . '/">' . $ort . '-' . $stadtteil . '</a></li>';
+		} else {
+			echo '<li class="wisyr_landingpage_list_ort"><a href="/orte/' . urlencode($ort) . '/">' . $ort . '</a></li>';
+		}
+	}
+	
+	/*
+	 * Details für einzelnen Ort ausgeben
+	 * Kursliste
 	 */
 	function renderOrtsDetail($ortsname, $stadtteilname) {
 		
 		$title = $ortsname;
 		$querystring = $ortsname;
 		if(trim($stadtteilname) != '') {
-			$title .= ' - ' . $stadtteilname;
+			$title .= '-' . $stadtteilname;
 			$querystring .= ',' . $stadtteilname;
 		}
 	
@@ -132,18 +144,8 @@ class WISY_LANDINGPAGE_RENDERER_CLASS
 	}
 	
 	/*
-	 * Einzelnen Ort ausgeben
-	 */
-	function renderOrt($ort, $stadtteil='') {
-		if(trim($stadtteil) != '') {
-			echo '<li class="wisyr_landingpage_list_ort_stadtteil"><a href="/orte/' . urlencode($ort) . '/' . urlencode($stadtteil) . '/">' . $ort . '-' . $stadtteil . '</a></li>';
-		} else {
-			echo '<li class="wisyr_landingpage_list_ort"><a href="/orte/' . urlencode($ort) . '/">' . $ort . '</a></li>';
-		}
-	}
-	
-	/*
 	 * SQL für Abfrage der Ortsliste zusammenbauen
+	 * Optional mit Berücksichtigung der stichworte für "Themen & Orte"
 	 *
 	 * Alle Orte finden, die:
 	 *	* Via stdkursfilter in diesem Portal erlaubt sind
@@ -152,16 +154,26 @@ class WISY_LANDINGPAGE_RENDERER_CLASS
 	 *	* In durchfuehrungen genutzt werden die:
 	 *		* Nicht abgelaufen sind
 	 *		* Einem freigeschalteten Kurs angehören -> 1 oder 4
+	 *
+	 * Falls ein Array mit Stichworten übergeben wird, werden nur Orte gefunden
+	 * wo Kurse mit diesen Stichworten stattfinden
+	 *
 	 */
-	private function getOrtslisteSql() {
+	private function getOrtslisteSql($stichworte=array()) {
 		$portal_tag_id = $this->getPortaltagId();
 		
 		// Allowed / denied PLZ für Portal
 		$plz_allow = str_replace(',empty', '', $this->framework->iniRead('durchf.plz.allow', ''));
 		$plz_deny = str_replace(',empty', '', $this->framework->iniRead('durchf.plz.deny', ''));
-		
-		$select_fields = 'durchfuehrung.ort';
-		$select_fields_outer = 'df.ort';
+				
+		// Nach Themen filtern?
+		if(count($stichworte)) {
+			$select_fields = 'stichwoerter.stichwort, stichwoerter.stichwort_sorted, durchfuehrung.ort';
+			$select_fields_outer = 'df.stichwort, df.stichwort_sorted, df.ort';
+		} else {
+			$select_fields = 'durchfuehrung.ort';
+			$select_fields_outer = 'df.ort';
+		}
 		
 		// Stadtteile anzeigen?
 		$stadtteile = false;
@@ -178,15 +190,18 @@ class WISY_LANDINGPAGE_RENDERER_CLASS
 		$select .= " LEFT JOIN kurse ON kurse.id=kurse_durchfuehrung.primary_id";
 		if(strlen(trim($portal_tag_id))) {
 			$select .= " LEFT JOIN x_kurse_tags ON kurse.id=x_kurse_tags.kurs_id";
-			$select .= " WHERE x_kurse_tags.tag_id=$portal_tag_id";
-		} else {
-			$select .= " WHERE 1=1";
+		}
+		if(count($stichworte)) {
+			$select .= " LEFT JOIN kurse_stichwort ON kurse_stichwort.primary_id=kurse.id";
+			$select .= " LEFT JOIN stichwoerter ON stichwoerter.id=kurse_stichwort.attr_id";
 		}
 		$select .= " AND (kurse.freigeschaltet = 1 OR kurse.freigeschaltet = 4)";
 		$select .= " AND durchfuehrung.ort != ''";
 		$select .= " AND (durchfuehrung.ende = '0000-00-00 00:00:00' OR durchfuehrung.ende >= '". strftime("%Y-%m-%d 00:00:00") . "')";
 		if(strlen(trim($plz_allow))) $select .= " AND durchfuehrung.plz IN (" . $plz_allow . ")";
 		if(strlen(trim($plz_deny))) $select .= " AND durchfuehrung.plz NOT IN (" . $plz_deny . ")";
+		if(strlen(trim($portal_tag_id))) $select .= " WHERE x_kurse_tags.tag_id=$portal_tag_id";
+		if(count($stichworte)) $select .= " AND stichwoerter.id IN(" . implode(',', $stichworte) . ")";
 		$select .= " GROUP BY $select_fields";
 		
 		// Filtere Orte anhand plz_ortscron
@@ -201,12 +216,151 @@ class WISY_LANDINGPAGE_RENDERER_CLASS
 	/*
 	 * Landing page für Themen & Orte
 	 *
-	 *
 	 */
 	function renderLandingpageThemen() {
 		
-		echo 'TODO Themen';
+		// Stichwort, Ort und Stadtteil aus request URI extrahieren
+		$urlparts = $this->getUrlParts();
+		if($urlparts) {
+			$thema_und_ortsname = trim(utf8_decode(urldecode($urlparts[1])));
+			$stadtteilname = trim(urldecode($urlparts[2]));
+			
+			$thema = '';
+			$ortsname = '';
+			if($thema_und_ortsname != '') {
+				$parts = explode('-in-', $thema_und_ortsname);
+				if(count($parts) == 2) {
+					$thema = $parts[0];
+					$ortsname = $parts[1];
+				}
+			}
+			
+			if($thema != '') {
+				$this->db->query("SELECT stichwort FROM stichwoerter WHERE stichwort LIKE ".$this->db->quote($thema). " LIMIT 1");
+				
+				$thema = '';
+				while( $this->db->next_record() ) {
+					$thema = $this->db->f8('stichwort');
+				}
+				$this->db->free();
+			}
+			
+			if($ortsname != '') {
+
+				$this->db->query("SELECT ort FROM plz_ortscron WHERE ort LIKE ".$this->db->quote($ortsname). " LIMIT 1");
+				
+				$ortsname = '';
+				while( $this->db->next_record() ) {
+					$ortsname = $this->db->f8('ort');
+				}
+				$this->db->free();
+			}
+		}
+		if($thema != '' && $ortsname != '')
+		{
+			// Details eines einzelnen Themas in Ort
+			$title = $thema . ' in ' . $ortsname;
+			if($stadtteilname != '') $title .= '-' . $stadtteilname;
+			echo $this->framework->getPrologue(array(
+				'title'	=> $title,
+				'bodyClass' => 'wisyp_search wisyp_landingpage_' . $this->type,
+			));
+			echo '<div id="wisy_resultarea" class="wisy_landingpage">';
+			$this->renderThemaDetail($thema, $ortsname, $stadtteilname);
+			echo '</div><!-- /#wisy_resultarea -->';
+			
+		} else {
+			// Liste aller Themen in Orten
+			echo $this->framework->getPrologue(array(
+				'title'		=>	'Alle Themen & Orte',
+				'bodyClass'	=>	'wisyp_search wisyp_landingpage_' . $this->type,
+			));
+			echo '<div id="wisy_resultarea" class="wisy_landingpage">';
+			
+			if(trim($this->framework->iniRead('seo.themen.stichworte')) != '')
+			{
+				$stichworte = $this->quotedArrayFromList(trim($this->framework->iniRead('seo.themen.stichworte')));
+				if(count($stichworte)) {
+					$sql = $this->getOrtslisteSql($stichworte);
+					$this->renderThemenliste($sql);
+				}
+			}
+			
+			echo '</div><!-- /#wisy_resultarea -->';
+		}
+	}
+	
+	/*
+	 * Liste aller Themen an Orten ausgeben
+	 */
+	function renderThemenliste($sql) {
+		$this->db->query($sql);
 		
+		$this->renderListtitle();
+		
+		echo '<section class="wisyr_landingpage_list wisyr_landingpage_list' . $this->type . '">';
+		echo '<ul>';
+		$themaort = '';
+		while( $this->db->next_record() ) {
+			// Für Orte mit Stadtteil zusätzlich einen Eintrag ohne Stadtteil ausgeben
+			if(trim($this->db->f8('stadtteil')) != '' && $this->db->f8('stichwort_sorted') . $this->db->f8('ort') != $themaort) {
+				$themaort = $this->db->f8('stichwort_sorted') . $this->db->f8('ort');
+				$this->renderThema($this->db->f8('stichwort'), $this->db->f8('stichwort_sorted'), $this->db->f8('ort'));
+			}
+			$this->renderThema($this->db->f8('stichwort'), $this->db->f8('stichwort_sorted'), $this->db->f8('ort'), $this->db->f8('stadtteil'));
+		}
+		echo '</ul>';
+		echo '</section>';
+		$this->db->free();	
+	}
+	
+	/*
+	 * Einzelnes Thema an Ort für Liste ausgeben
+	 */
+	function renderThema($stichwort, $stichwort_sorted, $ort, $stadtteil='') {
+		if(trim($stadtteil) != '') {
+			echo '<li class="wisyr_landingpage_list_thema_ort_stadtteil"><a href="/themen/' . urlencode($stichwort_sorted) . '-in-' . urlencode($ort) . '/' . urlencode($stadtteil) . '/">' . $stichwort . ' in ' . $ort . '-' . $stadtteil . '</a></li>';
+		} else {
+			echo '<li class="wisyr_landingpage_list_thema_ort"><a href="/themen/' . urlencode($stichwort_sorted) . '-in-' . urlencode($ort) . '/">' . $stichwort . ' in ' . $ort . '</a></li>';
+		}
+	}
+	
+	/*
+	 * Detail für Thema an Ort ausgeben
+	 * -> Kursliste
+	 */
+	function renderThemaDetail($thema, $ortsname, $stadtteilname) {
+		$title = $thema . ' in ' . $ortsname;
+		$querystring = $thema . ',' . $ortsname;
+		if(trim($stadtteilname) != '') {
+			$title .= '-' . $stadtteilname;
+			$querystring .= ',' . $stadtteilname;
+		}
+	
+		$offset = htmlspecialchars(intval($_GET['offset'])); if( $offset < 0 ) $offset = 0;
+		$baseurl = $_SERVER['REQUEST_URI'];
+		
+		$this->renderDetailtitle($title);
+	
+		$searcher =& createWisyObject('WISY_SEARCH_CLASS', $this->framework);
+		$searcher->prepare(mysql_real_escape_string($querystring));
+	
+		$searchRenderer = createWisyObject('WISY_SEARCH_RENDERER_CLASS', $this->framework);
+		$searchRenderer->renderKursliste($searcher, $querystring, $offset, false, $baseurl);
+	}
+	
+	/*
+	 * Kommaseparierte Liste in Array umwandeln,
+	 * dabei db->quote() darauf anwenden
+	 */
+	private function quotedArrayFromList($input) {
+		$output = array();
+		foreach(explode(',', $input) as $i) {
+			if(trim($i) != '') {
+				$output[] = $this->db->quote(trim($i));
+			}
+		}
+		return $output;
 	}
 	
 	/* Abschlüsse ---------------------------- */
@@ -258,6 +412,9 @@ class WISY_LANDINGPAGE_RENDERER_CLASS
 		}
 	}
 	
+	/*
+	 * Einzelnen Abschluss ausgeben mit, soweit vorhanden, Glossarseite und Kursliste
+	 */
 	function renderAbschlussDetail($abschluss, $glossar) {
 		
 		$title = $abschluss;
@@ -283,6 +440,9 @@ class WISY_LANDINGPAGE_RENDERER_CLASS
 		$searchRenderer->renderKursliste($searcher, $querystring, $offset, true, $baseurl);
 	}
 	
+	/*
+	 * Liste aller Abschlüsse ausgeben
+	 */
 	function renderAbschlussliste($sql) {
 		$this->db->query($sql);
 		
@@ -295,13 +455,24 @@ class WISY_LANDINGPAGE_RENDERER_CLASS
 		}
 		echo '</ul>';
 		echo '</section>';
-		$this->db->free();	
+		$this->db->free();
 	}
 	
+	/*
+	 * Einzelnen Abschluss für Liste ausgeben
+	 */
 	function renderAbschluss($stichwort, $stichwort_sorted) {
 		echo '<li class="wisyr_landingpage_list_abschluss"><a href="/abschluesse/' . urlencode($stichwort_sorted) . '/">' . $stichwort . '</a></li>';
 	}
 	
+	/*
+	 * SQL für Abschlussliste zusammenstellen
+	 *
+	 * Alle Stichworte finden die:
+	 *	* Die Abschlüsse sind (eigenschaften=1)
+	 *	* Kursen in diesem Portal zugeordnet sind
+	 *	* Via stdkursfilter in diesem Portal erlaubt sind
+	 */
 	private function getAbschlusslisteSql() {
 		$portal_tag_id = $this->getPortaltagId();
 		
@@ -321,6 +492,9 @@ class WISY_LANDINGPAGE_RENDERER_CLASS
 	
 	/* Hilfsfunktionen ---------------------- */
 	
+	/*
+	 * URL mittels REGEX in Bestandteile zerlegen entlang slashes "/"
+	 */
 	private function getUrlParts() {
 		preg_match('/^\/?[^\/]+\/?([^\/]+)\/?([^\/]*)/', $_SERVER['REQUEST_URI'], $temp);
 		return $temp;
@@ -366,7 +540,7 @@ class WISY_LANDINGPAGE_RENDERER_CLASS
 				echo '<h1 class="wisyr_landingpage_detail_title">Alle Kurse in ' . htmlspecialchars($detail) . '</h1>';
 				break;
 			case 'themen':
-				echo '<h1 class="wisyr_landingpage_detail_title">Alle Kurse für Thema & Ort ' . htmlspecialchars($detail) . '</h1>';
+				echo '<h1 class="wisyr_landingpage_detail_title">Alle Kurse für ' . htmlspecialchars($detail) . '</h1>';
 				break;
 			case 'abschluesse':
 				echo '<h1 class="wisyr_landingpage_detail_title">Abschluß ' . htmlspecialchars($detail) . '</h1>';
