d2bbaf40d7fffa48e83e4129f43f259b008c2e56
jomey <jomey@joergs-mbp>
2019-03-19 22:19:44 +0100

    Advanced caching, and gzipping for portal.css and core.css

--- core51/wisy-dump-renderer-class.inc.php
+++ core51/wisy-dump-renderer-class.inc.php
@@ -16,24 +16,66 @@ class WISY_DUMP_RENDERER_CLASS
 	
 	function render()
 	{
-		global $wisyPortalId;
-	
-		$db = new DB_Admin;
-	
-		if( $this->param['src'] == 'portal.css' )
-		{
-			$sql = "SELECT css FROM portale WHERE id=$wisyPortalId;";
-			$db->query($sql);
-			if( $db->next_record() )
-			{
-				$css = $db->f8('css');
-				header("Content-type: text/css");
-				header("Content-length: " . strlen($css));
-				headerDoCache();
-				echo $css;			
-			}
-			$db->free($sql);
-		}
+	    global $wisyPortalId;
+	    
+	    $db = new DB_Admin;
+	    
+	    if( $this->param['src'] == 'portal.css' )
+	    {
+	        $gzip = (strpos($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip') !== FALSE);
+	        
+	        $sql = "SELECT css, css_gz, date_modified FROM portale WHERE id=$wisyPortalId;";
+	        
+	        $db->query($sql);
+	        if( $db->next_record() )
+	        {
+	            $css = $db->f8('css');
+	            $css_gz = $db->f('css_gz');
+	            $date_modified = $db->f('date_modified');
+	            
+	            header("Content-type: text/css");
+	            header('Expires: '.gmdate('D, d M Y H:i:s \G\M\T', time() + (60 * 60 * 24 * 30))); // 1 month // headerDoCache();
+	            header('Last-Modified: '.gmdate('D, d M Y H:i:s \G\M\T', strtotime($date_modified)));
+	            
+	            if($gzip && trim($css_gz) != "") {
+	                header ("Content-Encoding: gzip");
+	                header("Content-length: " . strlen($css_gz));
+	                echo $css_gz;
+	            } else {
+	                header("Content-length: " . strlen($css));
+	                echo $css;
+	            }
+	            
+	        }
+	        $db->free($sql);
+	    } elseif( $this->param['src'] == 'core.css' )
+	    {
+	        $gzip = (strpos($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip') !== FALSE);
+	        
+	        
+	        $db->query($sql);
+	        if( $db->next_record() )
+	        {
+	            $css = $db->f8('css');
+	            $css_gz = $db->f('css_gz');
+	            $date_modified = $db->f('date_modified');
+	            
+	            header("Content-type: text/css");
+	            header('Expires: '.gmdate('D, d M Y H:i:s \G\M\T', time() + (60 * 60 * 24 * 30))); // 1 month // headerDoCache();
+	            header('Last-Modified: '.gmdate('D, d M Y H:i:s \G\M\T', strtotime($date_modified)));
+	            
+	            if($gzip && trim($css_gz) != "") {
+	                header ("Content-Encoding: gzip");
+	                header("Content-length: " . strlen($css_gz));
+	                echo $css_gz;
+	            } else {
+	                header("Content-length: " . strlen($css));
+	                echo $css;
+	            }
+	            
+	        }
+	        $db->free($sql);
+	    }
 	}
 }
 
