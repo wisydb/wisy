f50f9b145e7752d401bc71e606e99895b630a41d
jomey <jomey@joergs-mbp>
2020-02-01 13:27:43 +0100

    New course filter, 27h delay: don't display if foreign and not logged in
    ... and not online editing.
    
    + dont display word clouds while online editing

--- core51/wisy-kurs-renderer-class.inc.php
+++ core51/wisy-kurs-renderer-class.inc.php
@@ -1,13 +1,15 @@
 <?php if( !defined('IN_WISY') ) die('!IN_WISY');
 
-
+require_once('admin/config/codes.inc.php');
 
 class WISY_KURS_RENDERER_CLASS
 {
-	var $framework;
-	var $unsecureOnly = false;
-
-	function __construct(&$framework)
+    var $framework;
+    var $unsecureOnly = false;
+    var $h_before_coursefilter = 27; // we want to ignore GMT time zone + daylight saving time complications + usually not in Google index yet
+    var $h_before_dontshowteditorforeign_k = 27; // we want to ignore GMT time zone + daylight saving time complications + usually not in Google index yet
+    
+    function __construct(&$framework)
 	{
 		// constructor
 		$this->framework =& $framework;
@@ -20,8 +22,6 @@ class WISY_KURS_RENDERER_CLASS
 		global $wisyPortalId;
 
 		$kursId = intval($_GET['id']);
-		
-		$this->checkKursFilter($wisyPortalId, $kursId);
 
 		// query DB
 		$db = new DB_Admin();
@@ -43,6 +43,8 @@ class WISY_KURS_RENDERER_CLASS
 		$date_modified		= $db->f('date_modified');
 		$bu_nummer 			= PHP7 ? $db->f('bu_nummer') : $db->f8('bu_nummer');
 		$pflege_pweinst		= PHP7 ? intval($db->f('pflege_pweinst')) : intval($db->f8('pflege_pweinst'));
+		
+		$this->filter_foreign_k($db, $wisyPortalId, $kursId, $date_created);
 		
 		$anbieter_name = PHP7 ? $db->f('suchname') : $db->f8('suchname');
 		$anbieterdetails['suchname'] = $anbieter_name;
@@ -185,10 +187,10 @@ class WISY_KURS_RENDERER_CLASS
 				}
 						
 				/* // ... Bildungsurlaubsnummer 
-				if (($wisyPortalSpalten & 128) > 0)
+				if (($wisyPortalSpalten & 128) > 0 && $bu_nummer)
 				{
 					$rows .= '<dt>Bildungsurlaubsnummer:&nbsp;</dt>';
-					$rows .= '<dd>' .($bu_nummer? 'Ja' : 'Nein'). '</dd>';
+					$rows .= '<dd>ja</dd>';
 				} */
 
 				if( $rows != '' ) 
@@ -299,7 +301,8 @@ class WISY_KURS_RENDERER_CLASS
 			// vollstaendigkeit feedback, editieren etc.
 			echo '<footer class="wisy_kurs_footer">';
 			
-			if($this->framework->iniRead('sw_cloud.kurs_anzeige', 0)) {
+			// dont display word clouds while online editing
+			if( $this->framework->iniRead('sw_cloud.kurs_anzeige', 0) && !$this->framework->editSessionStarted ) {
 			    
 			    global $wisyPortalId;
 			    $cacheKey = "sw_cloud_p".$wisyPortalId."_k".$kursId;
@@ -423,36 +426,52 @@ class WISY_KURS_RENDERER_CLASS
 	
 		echo "\n</div><!-- /#wisy_resultarea -->";
 		
+		// ! $db->close();
+		
 		echo $this->framework->getEpilogue();
 	}
 	
-	function checkKursFilter($wisyPortalId, $kursId) {
-		
-		// If no filter, display course
-		if(!$GLOBALS['wisyPortalFilter']['stdkursfilter'] || trim($GLOBALS['wisyPortalFilter']['stdkursfilter']) == '')
-			return true;
-		
-		$portaltag = ".portal".$wisyPortalId;
-		
-		$db = new DB_Admin();
-		$tagsql = 'SELECT tag_id FROM x_tags WHERE tag_name="'.$portaltag.'"';
-		$db->query($tagsql);
-		
-		if( !$db->next_record() )
-			$this->framework->error404();
-			
-		$tagId_portal = $db->f('tag_id');
-			
-		if( !$tagId_portal )
-			$this->framework->error404();
-				
-		$kurssql = "SELECT DISTINCT kurse.id FROM kurse LEFT JOIN x_kurse ON x_kurse.kurs_id=kurse.id LEFT JOIN x_kurse_tags j0 ON x_kurse.kurs_id=j0.kurs_id "
-				  ."LEFT JOIN x_kurse_tags j1 ON x_kurse.kurs_id=j1.kurs_id  WHERE kurse.id = $kursId AND j1.tag_id=$tagId_portal";
-						
-		$db->query($kurssql);
-						
-		if( trim($this->framework->iniRead('seo.set404_fremdkurse', "")) == 1 && (!$db->next_record() || !$db->f('id'))) // && ini.read(fremdekurseausschliessen)
-		$this->framework->error404();
+	function filter_foreign_k(&$db, $wisyPortalId, $kursId, $date_created) {
+	    
+	    // if portal has no filter, display course
+	    if(!$GLOBALS['wisyPortalFilter']['stdkursfilter'] || trim($GLOBALS['wisyPortalFilter']['stdkursfilter']) == '')
+	        return true;
+	        
+	        // check if course in search index (=allowed by portal filter)
+	        $searcher2 =& createWisyObject('WISY_SEARCH_CLASS', $this->framework);
+	        $searcher2->prepare('kid:' . $kursId);
+	        $anzahlKurse = $searcher2->getKurseCount();
+	        
+	        if($_GET['debug'] == 10 && $anzahlKurse == 1) {
+	            echo "<br>Seite portaleigen!<br>";
+	        }
+	        
+	        if($anzahlKurse == 1)
+	            return true;
+	            
+	            // throw 404 error if filter active & visitor not logged in & course ceated one day ago or earlier
+	            $k_created = strtotime($this->framework->formatDatum($date_created));
+	            $k_min_lifespan = strtotime(date("Y-m-d H:i:s"))-(60*60*$this->h_before_coursefilter); // now - 27 hours (we want to ignore GMT time zone + daylight saving time)
+	            $k_oldenough = $k_created < $k_min_lifespan;
+	            
+	            $exclude_foreign_k = trim($this->framework->iniRead('seo.set404_fremdkurse', true));
+	            $filter_active = $exclude_foreign_k && $k_oldenough;
+	            
+	            if( trim($this->framework->iniRead('disable.kurse', false)) && !$this->framework->is_editor_active($db, $this->h_before_dontshowteditorforeign_k) && !$this->framework->is_frondendeditor_active() ) {
+	                $file = $_SERVER['DOCUMENT_ROOT'].'/kursportal.dat';
+	                $current = file_get_contents($file);
+	                $current .= $_SERVER['HTTP_REFERER']." - ".$_SERVER['QUERY_STRING']." - ".$_SERVER['REQUEST_URI']."\n";
+	                file_put_contents($file, $current);
+	                $this->framework->error404("Fehler 404 - Seite <i>in diesem Portal</i> nicht gefunden", "<ul><li><a href='/edit?action=ek&id=0'>Zur Seite wechseln: \"Onlinepflege-Login f&uuml;r Anbieter\" ...</a></li></ul>");
+	            }
+	            
+	            if($_GET['debug'] == 10) {
+	                echo "Anzahl Kurse: ".$anzahlKurse."<br>Exclude foreign Kurse:".$exclude_foreign_k."<br>Alt genug: ".$k_oldenough." <small><br>[created: ".date("d.m.Y H:i", $k_created)."<br>sp&auml;testens: ".date("d.m.Y H:i", $k_min_lifespan)."]</small><br>Editor active ".$this->framework->is_editor_active($db, $this->h_before_dontshowteditorforeign_k)."<br>Online-Pflege: ".intval($this->framework->is_frondendeditor_active())."<br>";
+	            }
+	            
+	            if($filter_active && !$this->framework->is_editor_active($db, $this->h_before_dontshowteditorforeign_k) && !$this->framework->is_frondendeditor_active()) // now - 27 hours (we want to ignore GMT time zone + daylight saving time)
+	                $this->framework->error404("Fehler 404 - Seite <i>in diesem Portal</i> nicht gefunden", "<ul><li><a href='/edit?action=ek&id=0'>Zur Seite wechseln: \"Onlinepflege-Login f&uuml;r Anbieter\" ...</a></li></ul>");
+	                
 	}
 };
 
